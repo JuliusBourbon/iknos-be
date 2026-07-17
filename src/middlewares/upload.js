efc8123c1065e09a1336b19f7a9f6d0d315b566b
JuliusBourbon <naherrrrr@gmail.com>
2026-07-17 13:08:58 +0700

    fix: multer imgae upload limits

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -4,7 +4,7 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
     storage,
-    limits: { fileSize: 20 * 1024 * 1024 }, // maksimal 5MB
+    limits: { fileSize: 10 * 1024 * 1024 }, // maksimal 10MB
     fileFilter: (req, file, cb) => {
         const allowedTypes = ['image/jpeg', 'image/png', 'image/webp'];
         if (!allowedTypes.includes(file.mimetype)) {
